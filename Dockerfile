607dfd2f2d605baacd233017f77cae2b7170b879
mbekmirzayev <bekmirzayevoff@gmail.com>
2026-01-06 23:37:57 +0500

    feat: add new models and APIs

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM ghcr.io/astral-sh/uv:python3.13-alpine
-
-WORKDIR /app
-
-COPY . .
-
-RUN uv sync
-
-EXPOSE 8000 8001
--- apps/view/__init__.py
+++
@@ -1,3 +0,0 @@
-from apps.view.auth import CustomLogoutView, CustomLoginAPIView, RegisterAPIView, VerifyCodeAPIView
-from apps.view.views import UserProfileViewSet, OrganizationListAPIView, CourseModelViewSet, CategoryModelViewSet, \
-    GroupModelViewSet, TeacherStudentList
